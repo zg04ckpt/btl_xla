1cf4b86749cf10c5f11d7138f7db52c506950f8f
hoangkhoi2k4 <lapyen30@gmail.com>
2025-12-01 08:15:39 +0700

    finish

--- application/preprocessing/__init__.py
+++ application/preprocessing/__init__.py
@@ -2,7 +2,7 @@
 Preprocessing package - Xử lý tiền xử lý cho các loại ký tự và hình học
 """
 
-from .base_preprocessor import BasePreprocessor
+# from .base_preprocessor import BasePreprocessor
 from .general_preprocessor import GeneralPreprocessor
 
 __all__ = [
